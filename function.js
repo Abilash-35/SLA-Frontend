c9ec6e1d37dee83dc5509ebf67ebfef0afebee18
Abilash-35 <85357831+Abilash-35@users.noreply.github.com>
2025-11-01 08:25:01 +0530

    done

---
+++ function.js
@@ -0,0 +1,16 @@
+// function abi(){
+//     console.log("Hi how are you");
+    
+// }
+// abi();
+
+// function sayHi(a,b){
+// return a + b
+// }
+
+// console.log(sayHi(10,2));
+
+const abi=()=>
+    console.log("Hi");
+
+abi(10,20);
